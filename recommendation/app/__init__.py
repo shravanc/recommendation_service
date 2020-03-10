d60d807996ec0a7c5bae91a8718d877a8e83341d
shravanc <shravan007.c@gmail.com>
2020-03-10 18:06:42 +0000

    postgres integration

--- recommendation/app/__init__.py
+++ recommendation/app/__init__.py
@@ -1,24 +1,39 @@
 from flask import Flask
-
+import os
 from flask_pymongo import PyMongo
+from flask_sqlalchemy import SQLAlchemy
+
 
 mongo = PyMongo()
 
+print("**************", os.path.abspath(os.path.dirname(__file__)))
+
+
 def create_app(config_filename=None):
   application = Flask( 'recommendation', instance_relative_config=True)
-  #application.config.from_pyfile(config_filename)
-  #initialize_extensions(application)
+  application.config.from_pyfile(config_filename)
+  initialize_extensions(application)
   register_blueprints(application)
   
   return application
 
-def initialize_extension(application):
+def initialize_extensions(application):
   mongo.init_app(application)
   from app.models.user import User
+  from app.models.rating import Rating  
+
+  db = SQLAlchemy(application)
 
 
 def register_blueprints(application):
   from app.controllers import recommends_blueprints
   application.register_blueprint(recommends_blueprints)
 
+
+path = os.path.abspath(os.path.dirname(__file__)) + "/../instance/development.cfg"
+print("path******>", path)
+app = Flask(__name__)
+app.config.from_pyfile(path)
+db = SQLAlchemy(app)
+#db = SQLAlchemy(create_app(path))
 
--- recommendation/app/controllers/recommends_controller.py
+++ recommendation/app/controllers/recommends_controller.py
@@ -1,9 +1,21 @@
 from flask import request, jsonify, render_template
+from app.models.user import User
+from app.models.rating import Rating
+
+from app import mongo
 
 
 def index():
-  data = {"data": {"recommends": "You got to wait for sometime"} }
-  return jsonify(data)
+  users = User.get()
+  user_ratings = Rating.query.all()
+  ratings = []
+  for rating in user_ratings:
+    d1 = {}
+    d1["user_id"] = rating.user_id
+    d1["item_id"] = rating.item_id
+    d1["rating"]  = rating.rating
+    ratings.append(d1)
+  return jsonify({'data': ratings})
 
 
 def home():
